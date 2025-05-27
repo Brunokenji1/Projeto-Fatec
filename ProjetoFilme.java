12ed50cb110aae596dba4f4bb15f12f83c91c55e
Bruno Kenji <obrunokenji@gmail.com>
2025-05-27 10:34:56 -0300

    Update and rename Jogo.java to ProjetoFilme.java

--- ProjetoFilme.java
+++ ProjetoFilme.java
@@ -2,7 +2,7 @@ package com.mycompany.colecaoenari;
 
 import java.util.Scanner;
 
-public class ProjetoFilm {
+public class ProjetoFilme {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
